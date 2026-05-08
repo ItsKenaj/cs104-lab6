fc9579ea74ea77f34944fdeb6f84728cd0e88120
Prakash Koukuntla <pkoukuntla123@gmail.com>
2026-05-08 15:28:57 -0700

    integrated random functions into main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
+#include "randfuncs.h"
 
 void printHelp() {
     std::cout << "Commands:\n";
     std::cout << "  help - show this message\n";
     std::cout << "  quit - exit the program\n";
+    std::cout << "  coin - flip a coin!\n";
+    std::cout << "  rollD6 - roll a die!\n";
+    std::cout << "  rollD20 - roll a D20!\n";
 }
 
 int main() {
@@ -18,6 +22,9 @@ int main() {
 
         if (cmd == "quit") break;
         else if (cmd == "help") printHelp();
+        else if (cmd == "coin") flipCoin();
+        else if (cmd == "rollD6") rollD6();
+        else if (cmd == "rollD20") rollD20();
         else std::cout << "Unknown command. Type 'help' for options.\n";
     }
     return 0;
